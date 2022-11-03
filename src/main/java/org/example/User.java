d5c29ad133793059c9d8367084d048a0ffbf2c7d
문다솜 <dsmoon@intube.kr>
2022-11-03 15:58:52 +0900

    User.java 설명 주석 추가

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -4,6 +4,12 @@ public class User {
     private String password;
 
     public void initPassword(PasswordGenerator passwordGenerator) {
+
+        // as-is
+        /*RandomPasswordGenerator randomPasswordGenerator = new RandomPasswordGenerator();
+        String password = randomPasswordGenerator.generatePassword();*/
+
+        // to-be : 결합을 느슨하게 만든다.
         String password = passwordGenerator.generatePassword();
         /**
          * 비밀번호는 최소 8자 이상 12자 이하여야 한다.
